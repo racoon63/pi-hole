26044bcbbcb818b9f222124e4873861b8d290421
Patrick Lubach <p.lubach@goreply.de>
2019-10-20 23:10:12 +0200

    added missed $

--- docker/setup.sh
+++ docker/setup.sh
@@ -6,20 +6,20 @@ read -s -p "Please enter your webpassword [supersecret123]: " PW
 echo
 read -p "Please enter the IP address of your pihole: " HOSTIP
 
-while HOSTIP == ""
+while $HOSTIP == ""
 do
-  read -p "Please enter the IP address of your pihole: " HOSTIP
+  read -p "Please enter the IP address of your pihole: " $HOSTIP
 done
 
 
 read -p "On which port should the pi-hole web interface run? [8000]: " WEBPORT
 
-if [ PW == "" ]
+if [ $PW == "" ]
 then
   PW="supersecret123"
 fi
 
-if [ WEBPORT == "" ]
+if [ $WEBPORT == "" ]
 then
   WEBPORT=8000
 fi
